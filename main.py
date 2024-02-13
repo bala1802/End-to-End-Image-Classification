be5c18e0791a4c1272159502711a7f49a7c875e9
bala1802 <bala1802@live.com>
2024-02-13 22:30:55 +0530

    data ingestion pipeline experiment added

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 from ChestCancerClassifier import logger
+from ChestCancerClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data Ingestion stage"
+
+
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
 
 logger.info("Welcome to Chest Cancer Classifier")
